c69c671bec0e3a5baf5182acf75b164ba386c6b2
Nishimura <fatenfarag0087@gmail.com>
2026-09-09 15:55:28 +0300

    feat/Sending invoice mail after success payment callback

--- app/Http/Controllers/StripePaymentCallbackController.php
+++ app/Http/Controllers/StripePaymentCallbackController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Actions\Stripe\VerifyStripeCheckoutSessionAction;
+use App\Actions\Stripe\HandleSuccessfulPaymentCallbackAction;
 use App\Http\Resources\Api\OrderPaymentResource;
 use App\Traits\V1\ApiResponse;
 use Illuminate\Http\JsonResponse;
@@ -13,18 +13,19 @@ class StripePaymentCallbackController extends Controller
 {
     use ApiResponse;
 
-    public function success(Request $request, VerifyStripeCheckoutSessionAction $action): JsonResponse
+    public function success(Request $request, HandleSuccessfulPaymentCallbackAction $action): JsonResponse
     {
         $sessionId = $request->query('session_id');
-        if (! $sessionId) {
+
+        if (! is_string($sessionId) || blank($sessionId)) {
             throw new InvalidArgumentException('Missing session_id parameter.');
         }
 
         $order = $action->execute($sessionId);
 
         return $this->successResponse(
-            'Payment successful. Your order has been placed.',
-            new OrderPaymentResource($order)
+            'Payment successful. Your order has been placed, and we are sending you an invoice email shortly.',
+            new OrderPaymentResource($order),
         );
     }
 
